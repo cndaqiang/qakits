e89cf7d8510a0362c2836183779c22e35e29af61
cndaqiang <who@cndaqiang.ac.cn>
2025-05-29 14:51:44 +0800

    env:USERPROFILE

--- localbuild.ps1
+++ localbuild.ps1
@@ -1,5 +1,5 @@
-& 'C:\Users\cnche\miniconda3\shell\condabin\conda-hook.ps1'
-conda activate 'C:\Users\cnche\miniconda3'
+& "$env:USERPROFILE\miniconda3\shell\condabin\conda-hook.ps1"
+conda activate "$env:USERPROFILE\miniconda3"
 Get-ChildItem -Path .\dist\* | Remove-Item
 python setup.py sdist
 Get-ChildItem -Path .\dist\* | ForEach-Object { python -m pip install $_.FullName }
